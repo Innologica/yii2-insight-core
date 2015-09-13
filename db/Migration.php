13759c2856b5b9b6b59c4011cdc33150a22ce7b9
Nikola Kostadinov <nikolakk@gmail.com>
2015-09-13 13:02:57 +0300

    added base migration

---
+++ db/Migration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Nikola Kostadinov<nikolakk@gmail.com>
+ * Date: 12.09.2015
+ * Time: 01:22 ч.
+ */
+
+namespace insight\core\db;
+
+
+class Migration extends \yii\db\Migration{
+
+}

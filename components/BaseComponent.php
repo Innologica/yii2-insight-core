ef2ef78927c79949956f41227d9e0e1e064d5a10
ntraikov <ntraikov@172.16.0.51>
2016-01-22 17:08:15 +0200

    IS-102 General Settings UI - Adding settings functionality

--- components/BaseComponent.php
+++ components/BaseComponent.php
@@ -2,15 +2,24 @@
 /**
  * @author Nikola Kostadinov<nikolakk@gmail.com>
  * Date: 21.12.2015
- * Time: 13:52 ÷.
+ * Time: 13:52 ï¿½.
  */
 
 namespace insight\core\components;
 
-
+use Yii;
 use yii\base\Component;
 
 class BaseComponent extends Component
 {
-
+    public function getSettings($key)
+    {
+        $value = Yii::$app->registry->get($key, Yii::$app->user->id);
+        if (!$value) {
+            if (isset(static::$settings[$key])) {
+                return static::$settings[$key];
+            }
+        }
+        return $value;
+    }
 }

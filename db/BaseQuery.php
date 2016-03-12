ad1330bfa1a4a7d1165c87c5912e5635fd9fac27
ntraikov <ntraikov@172.16.0.51>
2016-03-12 15:44:19 +0200

    Adding BaseQuery::oneOrThrow() method

--- db/BaseQuery.php
+++ db/BaseQuery.php
@@ -5,6 +5,9 @@ namespace insight\core\db;
 use yii\data\ActiveDataProvider;
 use yii\data\ArrayDataProvider;
 use yii\db\ActiveQuery;
+use yii\db\ActiveRecord;
+use yii\db\Connection;
+use yii\web\NotFoundHttpException;
 
 /**
  * @author Nikolay Traykov
@@ -29,5 +32,20 @@ class BaseQuery extends ActiveQuery
         return new ArrayDataProvider([
             'models' => $this->all(),
         ]);
+    }
+
+    /**
+     * @param Connection $db the DB connection used to create the DB command.
+     * @return ActiveRecord
+     * @throws NotFoundHttpException
+     */
+    public function oneOrThrow($db = null)
+    {
+        $model = parent::one($db);
+        if (!$model) {
+            throw new NotFoundHttpException();
+        }
+
+        return $model;
     }
 }

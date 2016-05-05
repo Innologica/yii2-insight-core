19c3a2188e67f520d655a739d81a1a9484b7381e
ntraikov <ntraikov@172.16.0.51>
2016-05-05 17:28:34 +0300

    Adding geonames helper

---
+++ util/GeoNames.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace insight\core\util;
+
+use yii\helpers\Json;
+
+/**
+ * @author Nikolay Traykov
+ */
+class GeoNames
+{
+    const BASE_URL = 'http://api.geonames.org/';
+    const USERNAME = 'innologica';
+
+    public static function timezone($lat, $lng)
+    {
+        $url = self::BASE_URL . 'timezoneJSON?username='.self::USERNAME."&lat=$lat&lng=$lng";
+        $result = self::sendRequest($url);
+        if (isset($result['status'])) { // Status is returned only when an error exists
+            return null;
+        }
+        return $result;
+    }
+
+    public static function countryInfo($countryCode)
+    {
+        $url = self::BASE_URL . 'countryInfoJSON?username='.self::USERNAME."&country=$countryCode";
+        $result = self::sendRequest($url);
+        if (!empty($result['geonames'])) { // 'geonames' is returned every time. It may be empty or not
+            return $result['geonames'][0];
+        }
+        return null;
+    }
+
+    private static function sendRequest($url)
+    {
+        return Json::decode(file_get_contents($url));
+    }
+}

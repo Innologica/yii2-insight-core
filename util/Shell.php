7738ebab12ed82159a2e818dcb5f181901bc2a24
ntraikov <ntraikov@172.16.0.51>
2016-05-26 16:41:03 +0300

    Code polishing

--- util/Shell.php
+++ util/Shell.php
@@ -7,25 +7,21 @@
 
 namespace insight\core\util;
 
-
 class Shell
 {
-
-    public static function launch($call) {
+    public static function launch($call)
+    {
         // Windows
-        if(BackgroundProcess::is_windows()){
-            pclose(popen("start /b $call", "r"));
+        if(self::isWindows()) {
+            pclose(popen("start $call", "r"));
         } else { // Some sort of UNIX
             pclose(popen($call." /dev/null &", "r"));
         }
         return true;
     }
 
-    public static function is_windows(){
-        if(PHP_OS == 'WINNT' || PHP_OS == 'WIN32'){
-            return true;
-        }
-        return false;
+    public static function isWindows()
+    {
+        return PHP_OS == 'WINNT' || PHP_OS == 'WIN32';
     }
-
 }

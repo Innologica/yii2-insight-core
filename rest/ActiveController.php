abbbd80bda16ba8f185ce0bc17b1355c7b52fde4
Nikola Kostadinov <nikolakk@gmail.com>
2017-01-18 10:54:46 +0200

    Add filter capabilities to Active REST controller

--- rest/ActiveController.php
+++ rest/ActiveController.php
@@ -2,6 +2,8 @@
 
 namespace insight\core\rest;
 
+use yii\data\ActiveDataProvider;
+use yii\helpers\Json;
 use yii\rest\ActiveController as BaseActiveController;
 
 /**
@@ -9,4 +11,32 @@ use yii\rest\ActiveController as BaseActiveController;
  */
 class ActiveController extends BaseActiveController
 {
+    public $filterParam = 'filter';
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        if(\Yii::$app->request->get('filter'))
+            $actions['index']['prepareDataProvider'] = function ($action) {
+                /* @var $modelClass \yii\db\BaseActiveRecord */
+                $modelClass = $this->modelClass;
+
+                $query = $modelClass::find();
+                $filters = \Yii::$app->request->get('filter');
+
+                foreach($filters as $filter => $params) {
+                    if(is_array($params)) {
+                        $query = call_user_func_array([$query, $filter], $params);
+                    } else {
+                        $query = call_user_func([$query, $filter]);
+                    }
+                }
+
+                return new ActiveDataProvider([
+                    'query' => $query,
+                ]);
+            };
+        return $actions;
+    }
+
 }

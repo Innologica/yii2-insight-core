47cec2935a7e17df629b3d7107785f09cfe55452
Nikola Kostadinov <nikolakk@gmail.com>
2017-03-08 19:20:58 +0200

    allow filtering via get parameters

--- rest/ActiveController.php
+++ rest/ActiveController.php
@@ -13,6 +13,20 @@ class ActiveController extends BaseActiveController
 {
     public $filterParam = 'filter';
 
+    /**
+     * Allow filtering by get parameter filter. Each item of the array is a method in the Finder object. If the
+     * element is array then the values of the array are passed as parameters.
+     *
+     * Example:
+     * {
+     *   "filter": [ "active", { "forId": { "id": 11 } }]
+     * }
+     *
+     * This will execute
+     * $model->find()->active()->forId(11)
+     *
+     * @return array
+     */
     public function actions()
     {
         $actions = parent::actions();
@@ -24,9 +38,10 @@ class ActiveController extends BaseActiveController
                 $query = $modelClass::find();
                 $filters = \Yii::$app->request->get('filter');
 
-                foreach($filters as $filter => $params) {
-                    if(is_array($params)) {
-                        $query = call_user_func_array([$query, $filter], $params);
+                foreach($filters as $filter) {
+                    if(is_array($filter)) {
+                        $function = key($filter);
+                        $query = call_user_func_array([$query, $function], $filter[$function]);
                     } else {
                         $query = call_user_func([$query, $filter]);
                     }

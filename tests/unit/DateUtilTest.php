ae447876b569cf25ce60fb9e15bf8c2ad1209a9c
Nikola Kostadinov <nikolakk@gmail.com>
2017-12-22 11:27:02 +0200

    add tests with vacations

--- tests/unit/DateUtilTest.php
+++ tests/unit/DateUtilTest.php
@@ -36,4 +36,17 @@ class DateUtilTest extends \Codeception\TestCase\Test
         $this->tester->assertEquals(2, $days, 'Working days should be 2');
     }
 
+    public function testVacationsDays()
+    {
+        $days = \insight\core\util\DateTimeUtil::getWorkingDays('2017-12-21', '2018-01-02 23:59:59', [
+            '2017-12-24', '2017-12-25', '2017-12-26', '2017-12-27',
+        ]);
+        $this->tester->assertEquals(6, $days, 'Working days should be 6');
+
+        $days = \insight\core\util\DateTimeUtil::getWorkingDays('2017-12-28', '2018-01-03 23:59:59', [
+            '2018-01-01',
+        ]);
+        $this->tester->assertEquals(4, $days, 'Working days should be 4');
+    }
+
 }
